72a5c2b4ab70a96224d0a4997bad6df7fe4eb90c
Fagun06 <mdfagun1357@gmail.com>
2024-09-06 00:09:29 +0600

    Completed Register

--- Inventory_System_MVC/Program.cs
+++ Inventory_System_MVC/Program.cs
@@ -1,7 +1,13 @@
+using Inventory_System_MVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<InventoryDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("InventoryConnectionString")));
 
 var app = builder.Build();
 
